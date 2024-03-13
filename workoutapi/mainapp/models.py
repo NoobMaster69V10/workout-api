b0fefdcf3a2fe1cbaffcd9406f4bbb3aad75246c
NoobMaster69V10 <giorgi.ubilava.1@btu.edu.ge>
2024-03-13 12:22:37 +0400

    Added custom user model

--- workoutapi/mainapp/models.py
+++ workoutapi/mainapp/models.py
@@ -1,3 +1,66 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, UserManager
+from django.utils import timezone
 
-# Create your models here.
+
+# Custom user model
+class CustomUserManager(UserManager):
+
+    def _create_user(self, username, password, **extra_fields):
+        if not username:
+            raise ValueError('Username must be set')
+
+        username = self.normalize_email(username)
+        user = self.model(username=username, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_user(self, username=None, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(username, password, **extra_fields)
+
+    def create_superuser(self, username=None, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self._create_user(username, password, **extra_fields)
+
+
+# Override default user class
+class User(AbstractBaseUser, PermissionsMixin):
+    username = models.CharField(max_length=255, unique=True)
+    email = models.EmailField(blank=True, default='')
+    name = models.CharField(max_length=255, blank=True, default='')
+    weight = models.SmallIntegerField(default=0, blank=True, null=True)
+    age = models.SmallIntegerField(default=0, blank=True, null=True)
+    height = models.SmallIntegerField(default=0, blank=True, null=True)
+
+    is_active = models.BooleanField(default=True)
+    is_superuser = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+
+    date_joined = models.DateTimeField(default=timezone.now)
+    last_login = models.DateTimeField(blank=True, null=True)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'username'
+    EMAIL_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
+
+    def get_full_name(self):
+        return self.name
+
+    def get_short_name(self):
+        return self.name
+
+
+#########################
+
+
