41bb7fb4a1b3a3fe130dad06d9c87f11576c49d8
NoobMaster69V10 <giorgi.ubilava.1@btu.edu.ge>
2024-03-13 12:30:34 +0400

    Added jwt authentication and serializer for user

--- workoutapi/mainapp/views.py
+++ workoutapi/mainapp/views.py
@@ -1,3 +1,61 @@
-from django.shortcuts import render
+from rest_framework.exceptions import AuthenticationFailed
+from rest_framework_simplejwt.tokens import RefreshToken
+from .serializer import UserSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import User
 
-# Create your views here.
+""" Authentication/Registration/Logout Views """
+
+
+class RegistrationView(APIView):
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response({"message": "User created successfully"})
+
+
+#########################
+class LoginView(APIView):
+    def post(self, request):
+        username = request.data['username']
+        password = request.data['password']
+
+        user = User.objects.filter(username=username).first()
+
+        if user is None:
+            raise AuthenticationFailed("User not found!")
+        if not user.check_password(password):
+            raise AuthenticationFailed("Incorrect password!")
+
+        refresh = RefreshToken.for_user(user)
+
+        response = Response()
+
+        response.set_cookie(key='jwt', value=str(refresh.access_token), httponly=True)
+        response.data = {
+            "jwt": str(refresh.access_token),
+            'refresh': str(refresh),
+        }
+
+        return response
+
+
+#########################
+class LogoutView(APIView):
+    def post(self, request):
+        if request.COOKIES.get('jwt') is not None:
+
+            response = Response()
+            response.delete_cookie('jwt')
+            response.data = {
+                'message': 'Successfully logged out'
+            }
+            return response
+        else:
+            return Response({'message': 'Not authenticated'})
+
+
+#########################
+
